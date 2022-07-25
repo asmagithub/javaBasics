28c379eaccbdf5032f05c2125351a0162f19af7f
asmab <asmab@DESKTOP-85EFU2E>
2022-07-24 22:59:49 -0400

    2nd commit

--- javaBasics/src/test/java/String/runner.java
+++ javaBasics/src/test/java/String/runner.java
@@ -4,7 +4,8 @@ public class runner {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first test");
-
+		System.out.println("This is asma");
+		System.out.println("This is by asma");
 	}
 
 }
